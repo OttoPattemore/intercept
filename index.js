7180acb71d020596af2961fb58d70836bd115b9f
Otto <57029490+OttoPattemore@users.noreply.github.com>
2022-04-05 17:04:59 +1000

    Add code

---
+++ index.js
@@ -0,0 +1,35 @@
+function calculateIntercept(line1M,line1C,line2M,line2C){
+    if((line1M-line2M) == 0){
+        return {
+            x: NaN,
+            y: NaN
+        }
+    }
+    const xIntercept = (line2C-line1C)/(line1M-line2M);
+    const yIntercept = line1M * xIntercept + line1C;
+    return {
+        x: xIntercept,
+        y: yIntercept
+    }
+}
+function calculate(){
+    const line1M = parseFloat(document.getElementById("l1slope").value);
+    const line1C = parseFloat(document.getElementById("l1intercept").value);
+    const line2M = parseFloat(document.getElementById("l2slope").value);
+    const line2C = parseFloat(document.getElementById("l2intercept").value);
+    const intercept = calculateIntercept(line1M,line1C,line2M,line2C);
+    const doLinesIntercept = intercept.x != NaN;
+
+
+
+    if(!doLinesIntercept){
+        document.getElementById("error_message").textContent = "The lines don't intercept!";
+        document.getElementById("x_intercept").textContent = "";
+        document.getElementById("y_intercept").textContent = "";
+    }else{
+        // Get the output elements
+        document.getElementById("x_intercept").textContent = "X Intercept = ".concat(intercept.x);
+        document.getElementById("y_intercept").textContent = "Y Intercept = ".concat(intercept.y);
+        document.getElementById("error_message").textContent = "";   
+    }
+}
